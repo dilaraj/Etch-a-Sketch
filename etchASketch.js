188e5245e10b0e13abc3e1937fcfbb24010ae1dd
Dilara Ajaj <106478364+dilaraj@users.noreply.github.com>
2024-10-10 08:56:05 +0000

    Making a customisable grid

--- etchASketch.js
+++ etchASketch.js
@@ -1,13 +1,33 @@
 let container = document.getElementById("container");
 
-createGrid = () => {
+createNewGrid = () => {
+    container.innerHTML = '';
 
-    for(let i = 0; i < 256; i++) {
-        var newColumn = document.createElement('div');
-        newColumn.className = "column";
-        container.appendChild(newColumn);
+    let gridSize = prompt("Create your new grid! How many many squares do you want on each side?")
+    if(gridSize > 100) {
+        alert("Number of squares cannot be greater than 100. Please try again!")
+        gridWidth = prompt("Create your new grid! How many squares wide?")
     }
-    
+
+    let finalSize = gridSize * gridSize;
+
+    let containerWidth = container.clientWidth;
+    let containerHeight = container.clientHeight;
+
+    let squareSize = containerWidth / gridSize;
+
+    for(let i = 0; i < finalSize; i++) {
+        var newSquare = document.createElement('div');
+        newSquare.className = "square";
+        container.appendChild(newSquare);
+    }
+
+    let squares = document.getElementsByClassName("square")
+
+    for (let square of squares) {
+        square.onmouseover = () => {
+            square.style.backgroundColor = "blue"
+        }
+    }
 }
 
-createGrid()
